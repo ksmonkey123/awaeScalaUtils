427de23612d38a85372453f5ea2d6309557e6998
Andreas Wälchli <andreas.waelchli@me.com>
2016-11-25 21:36:37 +0100

    removed self-type from Ref trait

--- src/ch/awae/scala/util/ref/Ref.scala
+++ src/ch/awae/scala/util/ref/Ref.scala
@@ -8,7 +8,6 @@ import ch.awae.scala.util.ref.RefType.SOFT
 import ch.awae.scala.util.ref.RefType.WEAK
 
 trait Ref[+A, T <: RefType[T]] extends (() => Option[A]) {
-  self =>
 
   def refType: RefType[T]
 
@@ -29,7 +28,7 @@ trait Ref[+A, T <: RefType[T]] extends (() => Option[A]) {
   def toWeak: Ref[A, WEAK]
 
   // STRIPPER
-  def stripped(update: Boolean = false): Ref[A, T] = self
+  def stripped(update: Boolean = false): Ref[A, T] = this
 }
 
 object Ref {
@@ -38,14 +37,14 @@ object Ref {
   def hard[A](f: () => A): Ref[A, HARD] = hard(f, f())
   def hard[A](f: () => A, value: A): Ref[A, HARD] =
     new RebuildingHardRef(f, value)
-  
+
   def soft[A](value: A): Ref[A, SOFT] = new SoftRef(value)
   def soft[A](f: () => A, force: Boolean = false): Ref[A, SOFT] =
     if (force) new RebuildingSoftRef(f, f())
     else new RebuildingSoftRef(f)
   def soft[A](f: () => A, value: A): Ref[A, SOFT] =
     new RebuildingSoftRef(f, value)
-  
+
   def weak[A](value: A): Ref[A, WEAK] = new WeakRef(value)
   def weak[A](f: () => A, force: Boolean = false): Ref[A, WEAK] =
     if (force) new RebuildingWeakRef(f, f())
