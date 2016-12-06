b7e10bfd003b9e61cd808a295f584cc671ddf27c
Andreas Wälchli <andreas.waelchli@me.com>
2016-12-06 11:31:34 +0100

    stuff

--- src/ch/awae/scala/util/ref/Ref.scala
+++ src/ch/awae/scala/util/ref/Ref.scala
@@ -7,18 +7,101 @@ import ch.awae.scala.util.ref.RefType.HARD
 import ch.awae.scala.util.ref.RefType.SOFT
 import ch.awae.scala.util.ref.RefType.WEAK
 
+/**
+ * Common base trait for all reference types. References are
+ * created by the companion object [[Ref$ Ref]]. All references are read-only,
+ * however immutability of the referenced object can obviously
+ * not be ensured.
+ *
+ * @tparam A the data type of the referenced object (covariant)
+ * @tparam T the [[RefType]] of the reference
+ *
+ * @define RECOVER For auto-recovering references this recreates the reference
+ * if it does not exist.
+ */
 trait Ref[+A, T <: RefType[T]] extends (() => Option[A]) {
 
+  /**
+   * the companion object to the reference type of this reference
+   * @return the companion object to the reference type of this reference
+   */
   def refType: RefType[T]
 
-  // ACCESSORS
+  /**
+   * retrieves the referenced object if it exists.
+   *
+   * @note $RECOVER
+   *
+   * @return a `Some` with the referenced object or `None` iff
+   * the object does not exist and cannot be recreated.
+   */
+  override def apply(): Option[A]
+
+  // ACCESSORS 
+
+  /**
+   * retrieves the referenced object. If it does not exist an exception is thrown.
+   *
+   * @note $RECOVER
+   *
+   * @return the referenced object if it exists
+   * @throws NoSuchElementException if the reference is empty
+   */
   @throws[NoSuchElementException]("if the reference is empty")
   def get: A
+
+  /**
+   * retrieves the referenced object or returns a default value if the referenced
+   * does not exist.
+   *
+   * @note $RECOVER Therefore the `default` value is ignored.
+   *
+   * @tparam B the type of the default value
+   * @param default the value to be used if the referenced object does not exist
+   * @return the referenced object or the `default` value iff the referenced object
+   * does not exist
+   */
   def getOrElse[B >: A](default: => B): B
+
+  /**
+   * returns this reference or an `alternative` iff this reference references an
+   * object that does not exist.
+   *
+   * @note for auto-recovering references this ''always'' returns the reference
+   * itself. The `alternative` is ignored.
+   *
+   * @tparam B the object type of the `alternative` reference
+   * @param alternative an alternative reference to be returned if this references
+   * an object that does not exist
+   */
   def orElse[B >: A](alternative: => Ref[B, T]): Ref[B, T]
+
+  /**
+   * Indicates if the referenced object exists.
+   *
+   * For auto-recovering references this indicates the actual state of the
+   * reference. Therefore if this returns `false` the next method that can
+   * recreate references ''will'' recreate the reference.
+   *
+   * @return `true` if the referenced object exists, `false` otherwise.
+   */
   def exists: Boolean
 
   // MONADIC FUNCTIONS
+  /**
+   * transforms the reference by applying a given function to the referenced
+   * object if it exists. The result from that function application is returned
+   * inside of a new reference.
+   *
+   * @note for auto-recovering references this does not recreate the referenced
+   * object and uses lazy object creation in the resulting reference object.
+   *
+   * @tparam B the type of the resulting reference
+   * @param f the transformation function to be applied
+   * @return a new reference containing the result of the application of the
+   * function `f` or this reference if it references an object that does not
+   * exist.
+   */
   def map[B](f: A => B): Ref[B, T]
   def flatMap[B](f: A => Ref[B, T]): Ref[B, T]
 
--- src/ch/awae/scala/util/ref/package.scala
+++ src/ch/awae/scala/util/ref/package.scala
@@ -6,17 +6,52 @@ import ch.awae.scala.util.ref.Ref
 import ch.awae.scala.util.ref.RefType
 import ch.awae.scala.util.ref.RefType._
 
+/**
+ * == Unified Reference Model for Scala ==
+ *
+ * There are 3 different supported types of references:
+ *   - Hard Reference (will not be cleared by GC)
+ *   - Soft Reference (will only be cleared when absolutely necessary)
+ *   - Weak Reference (can be cleared on any GC run)
+ *
+ * Normal references can't be recovered once they have been cleared.
+ * In addition there are also implementations for all reference types
+ * that carry a supplier function that is used internally to rebuild
+ * the reference if it has been cleared. This can be very useful for
+ * cache implementations.
+ *
+ * All reference types can be converted freely into all other reference
+ * types. For the auto-recovering references these conversions may be
+ * done lazily.
+ *
+ * References can be transformed using the `map` and `flatMap` functions.
+ * Auto-recovering references keep track of all mapping functions.
+ * Therefore a reference can be recovered even if any or all of its
+ * "parents" have been cleared.
+ */
 package object ref {
 
-  implicit class ReferenceDecoration[A](val item: A) extends AnyVal {
+  /**
+   * Implicit Wrapper providing reference creation functionality to any object
+   *
+   * @constructor creates a new [[ReferenceDecoration]] instance
+   * @tparam A the type of the decorated object
+   * @param item the object to decorate
+   */
+  implicit class ReferenceDecoration[A](private val item: A) extends AnyVal {
 
     /**
+     * @usecase def asRef[T <: RefType[T]]: Ref[A, T]
+     *
      * creates a reference of the given type containing the object.
-     * 
-     * {{{
+     * The reference type is passed as a type parameter.
+     *
+     * @tparam T the type of the reference to create
+     *
+     * @example {{{
      * import ref._
      * import ref.RefType._
-     * 
+     *
      * val x = List(1, 2, 3)
      * val h = x.asRef[HARD]
      * }}}
